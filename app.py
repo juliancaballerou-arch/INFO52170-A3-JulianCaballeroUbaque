01916d523089a4abc37e4bf2780e48b3b077c13a
juliancaballerou-arch <juliancaballerou@gmail.com>
2026-07-09 14:49:51 -0400

    Update greeting message - Firstname Lastname

--- app.py
+++ app.py
@@ -2,6 +2,10 @@
 def greet(name):
     return f"Hello, {name}! Welcome to INFO 52170."
 
+def info(name, class_name):
+    return f"{name} is culminating their studies in {class_name} this semester."
+
 if __name__ == "__main__":
     print(greet("Julian"))
+    print(info("Julian", "INFO 52170"))
 
